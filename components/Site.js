46a4d244f8482ceb8fbfad48b40a8989664830e1
Johannes Gorset <johannes.gorset@schibsted.com>
2020-07-11 17:48:06 +0200

    Design

--- components/Site.js
+++ components/Site.js
@@ -15,12 +15,19 @@ export default function Site(props) {
     <div>
       <div className="site">
         <div className="name">
-          <div className="value">{props.name}</div> is&nbsp;
+          <div className="value">{props.name}</div>
           <div className={"status " + props.status}>{props.status}</div>
         </div>
         <div className="details">
-          Up <div className="percent">{percent}%</div> last 30 days
-          (down <div className="downtime">{props.downtime}</div> min)
+          <div className="detail uptime">
+            <div className="label">Up</div>
+            <div className="value">{percent}%</div>
+          </div>
+
+          <div className="detail downtime">
+            <div className="label">Down</div>
+            <div className="value">{props.downtime} min</div>
+          </div>
         </div>
       </div>
 
@@ -33,13 +40,14 @@ export default function Site(props) {
           align-items: center;
           padding: 25px;
           margin-bottom: 15px;
-          border-bottom: 2px solid rgba(0, 0, 0, 0.2);
+          border-bottom: 2px solid rgba(0, 0, 0, 0.15);
           background: white;
           border-radius: 5px;
           font-size: 1.3rem;
         }
 
-        .name {
+        .name .value {
+          min-width: 12rem;
         }
 
         .name .value,
@@ -53,24 +61,64 @@ export default function Site(props) {
         }
 
         .status {
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
+        }
+
+        .status {
           background: green;
           color: #fff;
         }
 
         .details {
-          width: 50%;
-          text-align: right;
-          display: inline-block;
+          display: flex;
+          flex-direction: row-reverse;
+          flex-wrap: nowrap;
+          align-items: center;
+          font-size: 1rem;
+          flex-direction: row;
+          flex-wrap: nowrap;
+          justify-content: space-between;
+          align-items: center;
         }
 
-        .details .percent,
+        .details .uptime,
         .details .downtime {
           font-weight: bold;
           display: inline-block;
-          background: rgba(0, 0, 0, 0.04);
-          padding: 5px 7px;
-          border-bottom: 2px solid rgba(0, 0, 0, 0.08);
           border-radius: 3px;
+        }
+
+        .details .detail {
+          margin-right: 1rem;
+        }
+
+        .details .detail:last-of-type {
+          margin-right: 0;
+          border-right: 0;
+        }
+
+        .details .detail .label,
+        .details .detail .value {
+          padding: 5px 7px;
+          display: inline-block;
+          border-radius: 3px;
+        }
+
+        .details .detail .label {
+          border-bottom: 2px solid rgba(0, 0, 0, 0.08);
+          background: rgba(0, 0, 0, 0.04);
+          color: rgba(0, 0, 0, 0.6);
+          border-top-right-radius: 0;
+          border-bottom-right-radius: 0;
+        }
+
+        .details .detail .value {
+          border-bottom: 2px solid rgba(0, 0, 0, 0.95);
+          background: rgba(0, 0, 0, 0.75);
+          color: #fff;
+          border-top-left-radius: 0;
+          border-bottom-left-radius: 0;
         }
 
       `}</style>
