2d1ee936ff077237dc21ed095fe29464a2ed6b7a
Johannes Gorset <johannes.gorset@schibsted.com>
2020-07-12 15:30:11 +0200

    Style awesome meter percent

--- components/AwesomeMeter.js
+++ components/AwesomeMeter.js
@@ -32,7 +32,7 @@ export default memo(({ sites }) => {
   }).reduce((sum, value) => sum + value, 0) / sites.length;
 
   let header = (
-      <h1 className="awesome-meter">Everything is {avg.toFixed(3)}% {isItAwsome(avg)}</h1>
+      <h1 className="awesome-meter">Everything is <div className="percent">{avg.toFixed(3)}%</div> {isItAwsome(avg)}</h1>
   );
 
   if (!allUp(sites)) {
@@ -58,6 +58,14 @@ export default memo(({ sites }) => {
         }
         .red {
           color: red;
+        }
+        .percent {
+          font-weight: bold;
+          display: inline-block;
+          background: rgba(0, 0, 0, 0.2);
+          padding: 5px 9px;
+          border-bottom: 3px solid rgba(0, 0, 0, 0.3);
+          border-radius: 3px;
         }
       `}
       </style>
