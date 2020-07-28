65186545b8eb4183c5d4ed07a0475f4ae4699db7
Johannes Gorset <johannes.gorset@schibsted.com>
2020-07-28 12:51:49 +0200

    Set cache control for assets and pages, too

--- next.config.js
+++ next.config.js
@@ -9,6 +9,24 @@ module.exports = {
             value: 'public, must-revalidate, max-age=60'
           }
         ]
+      },
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, must-revalidate, max-age=60'
+          }
+        ]
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, must-revalidate, max-age=60'
+          }
+        ]
       }
     ]
   }
