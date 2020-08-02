9513923bca2b8ec43f50c229d30dee3080675efd
Johannes Gorset <johannes.gorset@schibsted.com>
2020-08-02 15:22:31 +0200

    Bounce the awesome meter

--- components/AwesomeMeter.js
+++ components/AwesomeMeter.js
@@ -14,11 +14,28 @@ const isItAwsome = (avg) => {
   }
 
   if(avg >= 99) {
-    return <strong className="green">awesome</strong>
+    return <strong className="green">
+      <div className="letter letter-1">a</div>
+      <div className="letter letter-2">w</div>
+      <div className="letter letter-3">e</div>
+      <div className="letter letter-4">s</div>
+      <div className="letter letter-5">o</div>
+      <div className="letter letter-6">m</div>
+      <div className="letter letter-7">e</div>
+    </strong>
   } else if(avg >= 90) {
-    return <strong className="green">okay</strong>
+    return <strong className="green">
+      <div className="letter letter-1">o</div>
+      <div className="letter letter-2">k</div>
+      <div className="letter letter-3">a</div>
+      <div className="letter letter-4">y</div>
+    </strong>
   } else {
-    return <strong className="red">bad</strong>
+    return <strong className="red">
+      <div className="letter letter-1">b</div>
+      <div className="letter letter-2">a</div>
+      <div className="letter letter-3">d</div>
+    </strong>
   }
 }
 
@@ -68,6 +85,31 @@ export default memo(({ sites }) => {
           border-bottom: 3px solid rgba(0, 0, 0, 0.3);
           border-radius: 3px;
           margin-bottom: 1rem;
+        }
+
+        .letter {
+          display: inline-block;
+          animation: bounce 5s infinite alternate ease-in-out;
+        }
+
+        .letter-1 { animation-delay: 0.0s }
+        .letter-2 { animation-delay: 0.1s }
+        .letter-3 { animation-delay: 0.2s }
+        .letter-4 { animation-delay: 0.3s }
+        .letter-5 { animation-delay: 0.4s }
+        .letter-6 { animation-delay: 0.5s }
+        .letter-7 { animation-delay: 0.6s }
+
+        @keyframes bounce {
+          0% {
+            transform: translate(0, 0);
+          }
+          95% {
+            transform: translate(0, 0);
+          }
+          100% {
+            transform: translate(0, -10px);
+          }
         }
       `}
       </style>
